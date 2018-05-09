afd5b26372c55a95fb9fbf24a2571477ef27e9b2
plaird730 <plaird730@gmail.com>
2018-05-09 18:20:44 -0400

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // INITIATE THE FOOTER
+  siteFooter();
+  $(window).resize(function() {
+    siteFooter();
+  });
+
+  // Parallax footer
+  function siteFooter() {
+    var siteContent = $("#site-content");
+    var siteContentHeight = siteContent.height();
+    var siteContentWidth = siteContent.width();
+
+    var siteFooter = $("#site-footer");
+    var siteFooterHeight = siteFooter.height();
+    var siteFooterWidth = siteFooter.width();
+
+    siteContent.css({
+      "margin-bottom": siteFooterHeight + 50
+    });
+  }
+});
