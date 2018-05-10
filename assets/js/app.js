56195f7ca4432829303df84d92ee8b73f4146ff1
plaird730 <plaird730@gmail.com>
2018-05-10 00:51:21 -0400

    Added parallax background, title, inspiring quote, about me with photo, portfolio with cards displaying page or source (github), fontawesome representation of tech used, footer, rook logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-  // INITIATE THE FOOTER
-  siteFooter();
-  $(window).resize(function() {
+$(document).ready(function () {
+    // INITIATE THE FOOTER
     siteFooter();
-  });
-
-  // Parallax footer
-  function siteFooter() {
-    var siteContent = $("#site-content");
-    var siteContentHeight = siteContent.height();
-    var siteContentWidth = siteContent.width();
-
-    var siteFooter = $("#site-footer");
-    var siteFooterHeight = siteFooter.height();
-    var siteFooterWidth = siteFooter.width();
-
-    siteContent.css({
-      "margin-bottom": siteFooterHeight + 50
+    $(window).resize(function () {
+        siteFooter();
     });
-  }
+
+    // Parallax footer
+    function siteFooter() {
+        var siteContent = $("#site-content");
+        var siteContentHeight = siteContent.height();
+        var siteContentWidth = siteContent.width();
+
+        var siteFooter = $("#site-footer");
+        var siteFooterHeight = siteFooter.height();
+        var siteFooterWidth = siteFooter.width();
+
+        siteContent.css({
+            "margin-bottom": siteFooterHeight + 40
+        });
+    }
 });
